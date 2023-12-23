0dae25225e40ae4fe80d3beaa7e04659ad1cf858
tired_i <ramen4598@naver.com>
2023-12-23 23:41:07 +0900

    UploadFileService 수정 : 운영체제에 따라서 저장 위치 변경

--- project/BookRentalPjt/src/main/java/com/office/library/book/admin/util/UploadFileService.java
+++ project/BookRentalPjt/src/main/java/com/office/library/book/admin/util/UploadFileService.java
@@ -9,19 +9,35 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public class UploadFileService {
 
+	final static public String BOOK_THUMBNAIL_UPLOAD_DIR = makeUploadDir();
+
+	static public String makeUploadDir() {
+		String osName = System.getProperty("os.name").toLowerCase();
+		if (osName.contains("win")) {
+			return "C:\\library\\upload\\";
+        } else if (osName.contains("mac")) {
+        	return System.getProperty("user.home") + "/Desktop/Study_JavaSpring_2/download/upload";
+        } else if (osName.contains("nix") || osName.contains("nux") || osName.contains("aix")) {
+        	return System.getProperty("user.home") + "/Desktop/Study_JavaSpring_2/download/upload";
+        } else {
+			return null;
+    	}
+	}
+
 	public String upload(MultipartFile file) {
 		
 		boolean result = false;
 		
 		String fileOriName = file.getOriginalFilename();
 		String fileExtension = fileOriName.substring(fileOriName.lastIndexOf("."), fileOriName.length());
-		String uploadDir = "C:\\library\\upload\\";
-		
+		if(BOOK_THUMBNAIL_UPLOAD_DIR == null) return null;
+
 		UUID uuid = UUID.randomUUID();
 		String uniqueName = uuid.toString().replaceAll("-", "");
 		
-		File saveFile = new File(uploadDir + "\\" + uniqueName + fileExtension);
-		
+		File saveFile = new File(BOOK_THUMBNAIL_UPLOAD_DIR + File.separator + uniqueName + fileExtension);
+		System.out.println("[UploadFileService]" + " UPLOAD_DIR : " + BOOK_THUMBNAIL_UPLOAD_DIR + " saveFile : " + saveFile.getAbsolutePath());
+
 		if(!saveFile.exists()) saveFile.mkdirs();
 		
 		try {
