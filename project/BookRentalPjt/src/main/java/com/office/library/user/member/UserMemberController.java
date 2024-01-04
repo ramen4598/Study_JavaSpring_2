b8d64878650ac258b3c3e1dfc00ba2b18433b307
tired_i <99334790+ramen4598@users.noreply.github.com>
2024-01-04 22:56:32 +0900

    사용자 회원가입 구현

--- project/BookRentalPjt/src/main/java/com/office/library/user/member/UserMemberController.java
+++ project/BookRentalPjt/src/main/java/com/office/library/user/member/UserMemberController.java
@@ -1,10 +1,37 @@
 package com.office.library.user.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user/member")
 public class UserMemberController {
 
+	@Autowired
+	UserMemberService userMemberService;
+
+	@GetMapping("/createAccountForm")
+	public String createAccountForm(){
+		System.out.println("[UserMemberController] createAccountForm()");
+
+		String nextPage = "user/member/create_account_form";
+
+	return nextPage;
+	}
+	
+	@PostMapping("/createAccountConfirm")
+	public String createAccountConfirm(UserMemberVo userMemberVo){
+		System.out.println("[UserMemberController] createAccountConfirm()");
+		String nextPage = "user/member/create_account_ok";
+
+		int result = userMemberService.createAccountConfirm(userMemberVo);
+	
+		if(result <= 0)
+			nextPage = "user/member/create_account_ng";
+
+		return nextPage;
+	}
 }
--- project/BookRentalPjt/src/main/java/com/office/library/user/member/UserMemberService.java
+++ project/BookRentalPjt/src/main/java/com/office/library/user/member/UserMemberService.java
@@ -1,8 +1,32 @@
 package com.office.library.user.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserMemberService {
+	
+	final static public int USER_ACCOUNT_ALREADY_EXIST = 0;
+	final static public int USER_ACCOUNT_CREATE_SUCCESS = 1;
+	final static public int USER_ACCOUNT_CREATE_FAIL = -1;
 
+	@Autowired
+	UserMemberDao userMemberDao;
+
+	public int createAccountConfirm(UserMemberVo userMemberVo){
+		System.out.println("[UserMemberService] createAccountConfirm()");
+		
+		boolean isMember = userMemberDao.isUserMember(userMemberVo.getU_m_id());
+		
+		if(!isMember){
+			int result = userMemberDao.insertUserAccount(userMemberVo);
+			
+			if(result > 0)
+				return USER_ACCOUNT_CREATE_SUCCESS;
+			else
+				return USER_ACCOUNT_CREATE_FAIL;
+		} else {
+			return USER_ACCOUNT_ALREADY_EXIST;
+		}	
+	}
 }
